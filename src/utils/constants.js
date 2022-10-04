7e1d5d4bcc9b03d0ccdb11f5870623cea2523cb2
Nivedita Chatterjee <niveditabarn21@gmail.com>
2022-10-04 16:08:21 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Competitive Programming', icon: <SchoolIcon />, },
+  { name: 'Web Development', icon: <SchoolIcon />, },
+  { name: 'Android Development', icon: <SchoolIcon />, },
+  { name: 'JS Mastery', icon: <SchoolIcon />, },
+  { name: 'Lama Dev', icon: <SchoolIcon />, },
+  { name: 'C++ coding', icon: <CodeIcon />, },
+  { name: 'JAVA', icon: <CodeIcon /> },
+  { name: 'Data Structures', icon: <CodeIcon />, },
+  { name: 'Graphs in C++', icon: <CodeIcon />, },
+  { name: 'Binary Trees in C++', icon: <CodeIcon />, },
+  { name: 'Stacks and Queues', icon: <CodeIcon />, },
+  { name: 'Linked Lists', icon: <CodeIcon />, },
+  { name: 'Bit manipulation', icon: <CodeIcon />, },
+  { name: 'GeeksforGeeks', icon: <SchoolIcon />, },
+  
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'Youtube';
+export const demoVideoTitle = 'Learn Youtube';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
